faf801a9a189c206684ed5ec5d0a7646f83c90b2
priscillamcamillo <priscillamc_ti>
2019-07-03 10:07:06 -0300

    Estrutura para layout pronta, boostrap aplicado, evento no botao add

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector("#btn-start").addEventListener("click", () => {
+  document.querySelector(".base-genrer").scrollIntoView({ behavior: "smooth" });
+});
+document.querySelector("#btn-next").addEventListener("click", () => {
+  document.querySelector(".base-time").scrollIntoView({ behavior: "smooth" });
+});
+document.querySelector("#btn-next_result_podcasts").addEventListener("click", () => {
+  document.querySelector(".base-result_podcasts").scrollIntoView({ behavior: "smooth" });
+});
